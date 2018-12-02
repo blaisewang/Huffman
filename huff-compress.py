140309cf37774d6a97e8fd7febe7bc6f7a350816
blaisewang <kaito.kaito.k@gmail.com>
2018-12-02 01:24:48 +0000

    add comments

--- huff-compress.py
+++ huff-compress.py
@@ -1,15 +1,15 @@
 import argparse
-import array
 import collections
 import operator
 import os
 import pickle
 import re
-import time
 
+from array import array
 from itertools import chain
 
 
+# Huffman tree node
 class Node:
     def __init__(self, symbol, frequency):
         self.symbol = symbol
@@ -20,12 +20,16 @@ class Node:
 
 class HuffmanTree:
     def __init__(self, plain_text):
+        # lookup dictionary
         self.lookup = {}
         self.text = plain_text
+        # symbols list, ascending order
         self.symbols = collections.Counter(self.text)
         self.symbols = sorted(self.symbols.items(), key=operator.itemgetter(1))
+        # Huffman tree node list, ascending order
         self.heap = list(chain(Node(symbol, frequency) for symbol, frequency in self.symbols))
 
+        # loop will stop iff Huffman tree construction completed
         while len(self.heap) > 1:
             left = self.heap.pop(0)
             right = self.heap.pop(0)
@@ -33,47 +37,68 @@ class HuffmanTree:
             merged = Node(None, left.frequency + right.frequency)
             merged.left, merged.right = left, right
 
+            # merged node insertion
             index = next((i for i, n in enumerate(self.heap) if n.frequency >= merged.frequency), -1)
             self.heap.insert(index if index != -1 else len(self.heap), merged)
 
-    def coding(self, node, code):
+    # Huffman tree to lookup dictionary
+    def encoding(self, node, code):
         if node.symbol:
+            # symbol found
             self.lookup[node.symbol] = code
             return
-        self.coding(node.left, code + "0")
-        self.coding(node.right, code + "1")
+        self.encoding(node.left, code + "0")
+        self.encoding(node.right, code + "1")
 
+    # Huffman tree to model dictionary
     def modeling(self, node):
         if node.symbol:
+            # symbol found
             return node.symbol
         return {0: self.modeling(node.left), 1: self.modeling(node.right)}
 
     def encoded(self):
-        self.coding(self.heap[0], "")
-        symbol_model = self.modeling(self.heap[0])
+        self.encoding(self.heap[0], "")
+        # text to encoded text
         coded_text = "".join(chain(self.lookup[symbol] for symbol in self.text))
+        # 0 padding
         coded_text = coded_text.ljust(len(coded_text) + 8 - len(coded_text) % 8, "0")
+        # encoded text to byte array
+        byte_array = array('B', list(chain(int(coded_text[i:i + 8], 2) for i in range(0, len(coded_text), 8))))
 
-        coded_array = array.array('B', list(chain(int(coded_text[i:i + 8], 2) for i in range(0, len(coded_text), 8))))
-
-        return coded_array, symbol_model
+        # return bin and model files
+        return byte_array, self.modeling(self.heap[0])
 
 
 if __name__ == '__main__':
+    # command line arguments
     parser = argparse.ArgumentParser(description="Huffman Coding Compression")
     parser.add_argument("infile", help="file to be compressed")
     parser.add_argument("-s", help="specify character-based (default) or word-based encoding", choices=["char", "word"])
     args = parser.parse_args()
 
+    # file name
     root, _ = os.path.splitext(args.infile)
+
+    # model of encoding, default character-based
     encoding_model = "char" if not args.s else args.s
 
-    text = open(args.infile, encoding="utf8").read() + "\x07"
+    # append BELL (pseudo-EOF) to the text
+    with open(args.infile, encoding="utf-8") as file:
+        text = file.read() + "\x07"
+
+    # char to word encoding model
     if encoding_model == "word":
         text = re.compile(r"[a-z]+|[^a-z]", re.I).findall(text)
 
+    # Huffman tree construction
     tree = HuffmanTree(text)
-    encoded_array, model = tree.encoded()
+    coded_array, model = tree.encoded()
 
-    encoded_array.tofile(open(root + ".bin", mode="wb"))
-    pickle.dump(model, open(root + "-symbol-model.pkl", mode="wb"))
+    # bin file store
+    with open(root + ".bin", mode="wb") as file:
+        coded_array.tofile(file)
+
+    # model file store
+    with open(root + "-symbol-model.pkl", mode="wb") as file:
+        pickle.dump(model, file)
--- huff-decompress.py
+++ huff-decompress.py
@@ -1,30 +1,41 @@
 import argparse
 import os
 import pickle
-import time
 
 from itertools import chain
 
 if __name__ == '__main__':
+    # command line arguments
     parser = argparse.ArgumentParser(description="Huffman Coding Decompression")
     parser.add_argument("bin", help="bin file to be decompressed")
     args = parser.parse_args()
 
-    coded_bytes = open(args.bin, mode='rb').read()
-    coded_text = "".join(chain('{0:0b}'.format(byte).zfill(8) for byte in coded_bytes))
+    # bin file read
+    with open(args.bin, mode="rb") as file:
+        coded_bytes = file.read()
 
+    # byte to string
+    coded_text = "".join(chain("{0:0b}".format(byte).zfill(8) for byte in coded_bytes))
+
+    # model file read
     root, _ = os.path.splitext(args.bin)
-    model = pickle.load(open(root + "-symbol-model.pkl", mode='rb'))
+    with open(root + "-symbol-model.pkl", mode="rb") as file:
+        model = pickle.load(file)
 
     text = ""
-    current = model
+    node = model
+    # coded text to original text
     for binary in coded_text:
-        current = current[0] if binary == "0" else current[1]
+        node = node[0] if binary == "0" else node[1]
 
-        if current == "\x07":
+        if node == "\x07":
+            # pseudo-EOF BELL found
             break
-        elif isinstance(current, str):
-            text += current
-            current = model
+        elif isinstance(node, str):
+            # leaf reached
+            text += node
+            node = model
 
-    open(root + "-decompressed.txt", mode="w", encoding="utf8").write(text)
+    # decompressed text store
+    with open(root + "-decompressed.txt", mode="w", encoding="utf-8") as file:
+        file.write(text)
